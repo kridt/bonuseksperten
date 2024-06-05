f0d4c52345c86e953fc94809e24f81781c0ce36a
Christian Nielsen <chrnielsen2003@gmail.com>
2024-06-05 07:49:21 +0200

    andmeldelser

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import Andmeldelser from "./pages/Andmeldelser";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <LandingPage />,
+    },
+    {
+      path: "/votes",
+      element: <Andmeldelser />,
     },
   ]);
 
